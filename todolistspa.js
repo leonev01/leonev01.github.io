695ab2401f1ee22f70156078d07de73b4290ab22
leonev01 <leonev01@users.noreply.github.com>
2015-10-19 13:03:53 -0500

    Update todolistspa.js

--- todolistspa.js
+++ todolistspa.js
@@ -1,12 +1,8 @@
 <html>
 <body>
 <script type="text/javascript">
-  add= function() {
-    var x;
-    document.querySelector('#box').innerHTML = document.querySelector('#text').value;
-  x= document.createElement("box")
-  x.type="checkbox"
-  x.id="text"
+  add = function () {
+    var list =document.querySelector('box').innerHTML = document.querySelector('text').value;
   }
 </script>
 </body>
