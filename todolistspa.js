542e6cdd0715c83c90872a0f04974712ee90fcca
leonev01 <leonev01@users.noreply.github.com>
2015-10-21 13:02:45 -0500

    Update todolistspa.js

--- todolistspa.js
+++ todolistspa.js
@@ -2,8 +2,21 @@
 <body>
 <script type="text/javascript">
   add = function () {
-    var list =document.querySelector('box').innerHTML = document.querySelector('text').value;
+    var list =document.querySelector("#box").innerHTML = document.querySelector("#textbox".value);
   }
+ 
+  level= document.querySelector("#level")
+  box= document.querySelector("#textbox")
+  ul= document.querSelecotr("#box")
+   li= document.createElement("li")
+      li.classList.add(level.value)
+   input= document.createElement("INPUT")
+      input.type("checkbox")
+   text= document.createTextNode(box.value)
+      ul.appendChild(li)
+      li.appendChild(input)
+      li.appendChild(text)
 </script>
+<script type="text/javascript" src="http://bnmnetp.github.io/cs130/listsaver.js"></script>
 </body>
 </html>
