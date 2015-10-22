267aeb673ef9579cc7a8e6d315463d7cfe18e6da
leonev01 <leonev01@users.noreply.github.com>
2015-10-22 16:44:41 -0500

    Update todolistspa.js

--- todolistspa.js
+++ todolistspa.js
@@ -1,21 +1,27 @@
 <html>
 <body>
 <script type="text/javascript">
+  done = function() {
+    if (this.checked) {
+      this.parentNode.classList.add("done");
+    }else{
+     this.parentNode.classList.remove("done"); 
+    }
   add = function () {
-    var list =document.querySelector("#box").innerHTML = document.querySelector("#textbox".value);
-  }
- 
-  level= document.querySelector("#level")
-  box= document.querySelector("#textbox")
-  ul= document.querSelecotr("#box")
-   li= document.createElement("li")
-      li.classList.add(level.value)
-   input= document.createElement("INPUT")
-      input.type("checkbox")
-   text= document.createTextNode(box.value)
-      ul.appendChild(li)
-      li.appendChild(input)
-      li.appendChild(text)
+    var list;
+  textbox= document.querySelector("#textbox");
+  ul= document.querSelecotr("#box");
+  level= document.querySelector("#level").value;
+  text= textbox.value;
+   li= document.createElement('li');
+      li.classList.add(level).value;
+   input= document.createElement('input');
+      input.type("checkbox");
+      input.input= done;
+   text= document.createTextNode(textbox).value;
+      ul.appendChild(li);
+      li.appendChild(input);
+      li.appendChild(text);
 </script>
 <script type="text/javascript" src="http://bnmnetp.github.io/cs130/listsaver.js"></script>
 </body>
