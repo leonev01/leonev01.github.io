4d68b029acbbf5eb68fad187e417c67db5dc756a
leonev01 <leonev01@users.noreply.github.com>
2015-10-23 10:13:06 -0500

    Update todolistspa.js

--- todolistspa.js
+++ todolistspa.js
@@ -5,35 +5,9 @@ done = function() {
 	}else {
 		this.parentNode.classList.remove("done");
 		}
-	localSave()
+
 }
-localSave = function() {
-	var res = []
-	var text;
-	entries = document.querySelectorAll('li')
-	for(text=0; text < entries.length; text++) {
-		if (! entries[text].classList.contains("done")) {
-		res.push(entries[text].innerText);
-		}
-	}
-	localStorage.setItem("todoDatabase",JSON.stringify(res))
-}
-restore = function () {
-	entries = JSON.parse(localStorage.getItem('todoDatabase'))
-	for (text=0; text < entries.length; text++){
-	
-	ul = document.querySelector("#box");
-	li = document.createElement('li');
-	input= document.createElement("input");
-	input.type = 'checkbox';
-	input.onclick = done;	
-	li.appendChild(input);
-	task= document.createTextNode(entries[text]);
-	li.appendChild(task);
-	ul.appendChild(li);
-	}
-	
-}
+
 add= function() {
 	var newli;
 	text = document.querySelector("#textbox");
@@ -50,11 +24,10 @@ add= function() {
 	li.appendChild(task);
 	ul.appendChild(li);
 	
-	
-localSave ()
+
 	
 }
-window.onload = restore;
+
 
 
 
