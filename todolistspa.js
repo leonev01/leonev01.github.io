aecbe81ad9e64c77d0069c71d035f150cee7d5df
leonev01 <leonev01@users.noreply.github.com>
2015-10-23 01:16:00 -0500

    Update todolistspa.js

--- todolistspa.js
+++ todolistspa.js
@@ -1,60 +1,33 @@
 <html>
 <body>
+<script type="text/javascript" src="http://bnmnetp.github.io/cs130/listsaver.js"></script>
 <script type="text/javascript">
-  done = function() {
-    if (this.checked) {
-      this.parentNode.classList.add("done");
-    }else{
-     this.parentNode.classList.remove("done"); 
-    }
-    localsave()
-  }
-  localSave = function() {
-	var res = []
-	var textbox;
-	allEntries = document.querySelectorAll('li')
-	for(textbox=0; textbox < allEntries.length; textbox++) {
-		if (! allEntries[textbox].classList.contains("done")) {
-		res.push(allEntries[textbox].innerText);
+done = function() {
+	if (this.checked){
+	this.parentNode.classList.add("done");
+	}else {
+		this.parentNode.classList.remove("done");
 		}
-	}
-	localStorage.setItem("todoDatabase",JSON.stringify(res))
 }
-restoreTask = function () {
-	allEntries = JSON.parse(localStorage.getItem('todoDatabase'))
-	for (textbox=0; textbox < allEntries.length; textbox++){
-	
+add= function() {
+	var newli;
+	text = document.querySelector("#textbox");
 	ul = document.querySelector("#box");
+	level= document.querySelector("#level").value;
+	word = text.value;
 	li = document.createElement('li');
+	li.className = level;
 	input= document.createElement("input");
 	input.type = 'checkbox';
 	input.onclick = done;	
 	li.appendChild(input);
-	text= document.createTextNode(allEntries[textbox]);
-	li.appendChild(text);
+	task= document.createTextNode(word);
+	li.appendChild(task);
 	ul.appendChild(li);
-	}
+	
+	
 	
 }
-  add = function () {
-    var list;
-  textbox= document.querySelector("#textbox");
-  ul= document.querSelecotr("#box");
-  level= document.querySelector("#level").value;
-  text= textbox.value;
-   li= document.createElement('li');
-      li.classList.add(level).value;
-   input= document.createElement('input');
-      input.type("checkbox");
-      input.input= done;
-   text= document.createTextNode(textbox).value;
-      ul.appendChild(li);
-      li.appendChild(input);
-      li.appendChild(text);
-      
-    localsave()
-  }
-  window.onload:restoreTask;
 </script>
 
 </body>
